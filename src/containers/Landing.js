11146da6cc41a13ae271c4698b1de3b53391ab8b
Ian Huang <21ianh1@students.tas.tw>
2021-03-12 14:12:04 +0800

    feat: landing for now

---
+++ src/containers/Landing.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import Logo from '../assets/logo2.png';
+
+const Wrapper = styled.div`
+  background: #f3f8fe;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+
+  & img {
+    width: 200px;
+    height: 200px;
+  }
+
+  & h1 {
+    color: rgb(25, 59, 104);
+  }
+
+  & p {
+    color: rgb(0, 110, 255);
+    font-size: 1.2em;
+    margin-top: 1em;
+  }
+`;
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <img src={Logo} alt="Walk For Refugees" />
+      <h1>Walk for Refugees sign-ups are beginning MONDAY, 3/15.</h1>
+      <p>Check back then to learn about and support our cause!</p>
+    </Wrapper>
+  );
+};
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -1,69 +1,80 @@
 import React from 'react';
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import ReactDOM from 'react-dom';
 
-import App from './containers/App';
-import { AuthProvider } from './contexts/authContext';
-import './index.css';
+import Landing from './containers/Landing';
 import reportWebVitals from './reportWebVitals';
-import { getToken } from './utils';
+import './index.css';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-  credentials: 'same-origin',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = getToken();
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          pledges: {
-            merge(_, incoming) {
-              return incoming;
-            },
-          },
-          me: {
-            merge(_, incoming) {
-              return incoming;
-            },
-          },
-        },
-      },
-    },
-  }).restore({}),
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </ApolloProvider>,
-  document.getElementById('root'),
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<Landing />, document.getElementById('root'));
 reportWebVitals();
+
+// import React from 'react';
+
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+// import ReactDOM from 'react-dom';
+
+// import App from './containers/App';
+// import { AuthProvider } from './contexts/authContext';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+// import { getToken } from './utils';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+//   credentials: 'same-origin',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = getToken();
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache({
+//     typePolicies: {
+//       Query: {
+//         fields: {
+//           pledges: {
+//             merge(_, incoming) {
+//               return incoming;
+//             },
+//           },
+//           me: {
+//             merge(_, incoming) {
+//               return incoming;
+//             },
+//           },
+//         },
+//       },
+//     },
+//   }).restore({}),
+// });
+
+// ReactDOM.render(
+//   <ApolloProvider client={client}>
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
+//   </ApolloProvider>,
+//   document.getElementById('root'),
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
