cdf599bdaced4068a19db3035b54712335dcbebe
Ian Huang <21ianh1@students.tas.tw>
2021-04-14 11:48:01 +0800

    feat: new email

--- server/src/graphql/resolvers/Query.js
+++ server/src/graphql/resolvers/Query.js
@@ -1,5 +1,21 @@
 import { PledgeModel, UserModel } from '../../models';
 
+// (async () => {
+//   const { received } = await UserModel.findOne({
+//     email: '23enyat@students.tas.tw',
+//   });
+
+//   for (const a of received) {
+//     const pledge = await PledgeModel.findById(a);
+//     // console.log(pledge.pledger);
+//     const pledger = await UserModel.findById(pledge.pledger);
+//     console.log(pledger);
+//     if (pledger.email === '23dylans@students.tas.tw') {
+//       console.log(pledge._id, pledge.flatDonation, pledge.perLapDonation);
+//     }
+//   }
+// })();
+
 const Query = {
   me(_, __, { user }) {
     return user;
